774f094e1c37a32e1de24cf5ce44e449601872c7
kcjacobs13 <keenanjacobs0213@gmail.com>
2023-03-11 16:12:29 -0600

    Function to display json data

---
+++ MidTermProject/code.js
@@ -0,0 +1,31 @@
+
+//Fetching the json file
+fetch('./data.json')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error:' + err);
+            })
+//This is a function that will appened data like a list
+function appendData(data) {
+    let mainContainer = document.getElementById("myData");
+    for (let serviceName in data) { //Each element in the json file
+        let div = document.createElement("div");
+        div.innerHTML = `<br> <br> <h1> ${serviceName} </h1>`;  //This is what is actually displayed onto the computer
+                                                                //Yet to be styled using CSS or JavaScript
+        mainContainer.appendChild(div);
+        let mainContainer2 = document.getElementById("myProduct"); //Next part of list
+    for (let element of data[serviceName]) {
+        console.log(element);
+        let div2 = document.createElement("div");
+        div2.innerHTML = `<h2>${element["serviceName"]}</h2> <br> ${element["information"]} <br> ${element["images"]}`;
+        mainContainer.appendChild(div2);
+    }
+    
+    } // end of for
+    
+} // end of function appendData
