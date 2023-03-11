d32e45f5d3d95df9c2715f2d6c088f1d1b13e1cd
kcjacobs13 <keenanjacobs0213@gmail.com>
2023-03-11 17:33:50 -0600

    Work in progress but json data and images now

--- MidTermProject/code.js
+++ MidTermProject/code.js
@@ -16,14 +16,17 @@ function appendData(data) {
     for (let serviceName in data) { //Each element in the json file
         let div = document.createElement("div");
         div.innerHTML = `<br> <br> <h1> ${serviceName} </h1>`;  //This is what is actually displayed onto the computer
-                                                                //Yet to be styled using CSS or JavaScript
+                                                                //Can add style to h1
         mainContainer.appendChild(div);
         let mainContainer2 = document.getElementById("myProduct"); //Next part of list
     for (let element of data[serviceName]) {
         console.log(element);
         let div2 = document.createElement("div");
-        div2.innerHTML = `<h2>${element["serviceName"]}</h2> <br> ${element["information"]} <br> <img src = "./images/${element["image"]}" width="200px" height ="200px"> </img>`;
+        div2.innerHTML = `<h2>${element["serviceName"]}</h2> <br> ${element["information"]} <br> `; //Can add style to h2
         mainContainer.appendChild(div2);
+        for(let image of element["image"]){
+            div2.innerHTML += ` <img src = "./images/${image}" width="200px" height ="200px"> </img> <h3>${image[description]}</h3>` ; //Can add style to h3
+        }
     }
     
     } // end of for
