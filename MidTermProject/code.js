a05ff0b35135a6d048b81a6ca89e1eed8de4e0d5
kcjacobs13 <keenanjacobs0213@gmail.com>
2023-03-11 18:35:24 -0600

    More images added and descriptions

--- MidTermProject/code.js
+++ MidTermProject/code.js
@@ -15,14 +15,14 @@ function appendData(data) {
     let mainContainer = document.getElementById("myData");
     for (let serviceName in data) { //Each element in the json file
         let div = document.createElement("div");
-        div.innerHTML = `<br> <br> <h1> ${serviceName} </h1>`;  //This is what is actually displayed onto the computer
+        div.innerHTML = `<h1><br>${serviceName}<br> </h1>`;  //This is what is actually displayed onto the computer
                                                                 //Can add style to h1
         mainContainer.appendChild(div);
         let mainContainer2 = document.getElementById("myProduct"); //Next part of list
     for (let element of data[serviceName]) {
         console.log(element);
         let div2 = document.createElement("div");
-        div2.innerHTML = `<h2>${element["serviceName"]}</h2> <br> ${element["information"]} <br> <br>`; //Can add style to h2
+        div2.innerHTML = `<br><h2 id="serviceNameId">${element["serviceName"]}</h2> ${element["information"]} <br> <sec>Past services: </sec> ${element["past_services"]} <br>`; //Can add style to h2
         mainContainer.appendChild(div2);
         for(let image of element["image"]){
             div2.innerHTML += ` <img src = "./images/${image}" width="200px" height ="200px"> </img>` ; //Can add style to h3
