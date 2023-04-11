a41f21c9206641c476c45243387e2f4f4bf34c30
kcjacobs13 <keenanjacobs0213@gmail.com>
2023-04-11 17:03:46 -0500

    Added a cart variable that keeps track of items

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 //Code for incrementing and decrementing on button clicks
+class product{
+  constructor(name, price){
+    this.name = name;
+    this.price = price;
+  }
+}
+
 var clicks = 0;
 var totalCost = 0;
+var cart = [];
 
 function onClickIncJetski() {
   clicks += 1;
   totalCost += 200;
   document.getElementById("clicks").innerHTML = clicks;
-  document.getElementById("totalCost").innerHTML = totalCost;
+  cart.push(new product("jetski",200));
+  console.log(cart);
 };
 function onClickDecJetski() {
   if(clicks > 0){
     clicks -= 1;
     totalCost-= 200;
     document.getElementById("clicks").innerHTML = clicks;
-    document.getElementById("totalCost").innerHTML = totalCost;
+    for(let i =0; i< cart.length; i++){
+      if(cart[i].name == "jetski"){
+        if(i + 1 == cart.length){
+          cart.pop();
+        }else{
+          cart.splice(i, i+1);
+        }
+      }
+    }
+    console.log(cart);
   }
 };
