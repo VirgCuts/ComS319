dde1b91c4aa78d322658467bffbd3dd67d173317
VirgCuts <cvcuts@iastate.edu>
2023-04-12 02:32:08 -0500

    Implemented buggy search, fully implemented page swap and reformated the products

--- Assignment2/src/index.js
+++ Assignment2/src/index.js
@@ -49,35 +49,122 @@ export function ListProducts() {
       return newCount
     })
   };
-  
-  
-  //used in debugging shows items at bottom
-  const cartItems = cart.map((product) => (
-<div key={product.id}>
-<img class=
-"img-fluid" src={product.image} width={30} />
-{product.title}
-${product.price}
-</div>
-));
 
+  //Handles the search currently doesnt fix itself when values are deleted
+  const [query, setQuery] = useState('');
+  const search = (e) => {
+    setQuery(e.target.value);
+    const results = cartTotal.filter(eachProduct => {
+    if (e.target.value === "") return cartTotal;
+    return eachProduct.title.toLowerCase().includes(e.target.value.toLowerCase())
+    });
+    setCartTotal(results);
+    }
+
+  
+  /*
+  *utilizes displayPage to determine pages to swap to if no page swaps have been called yet will initialize by checking if display page === 0
+  *if true first sets catalog to display page as starting point is always catalog (needed in order to remove catalog). Then removes the catalog
+  *and replaces it with page choice and assigns displayPage with said choice.  
+  *
+  */
+  let displayPage = 0;
+  function pageSwap(pageNum){
+
+    if(pageNum === 1 && displayPage == 0) {
+      displayPage = document.getElementById("productCatalog");
+    }
+    else if (pageNum === 1){
+      displayPage.style.display = "none";
+      displayPage = document.getElementById("productCatalog");
+      displayPage.style.display = "block";
+    }
+
+    if(pageNum === 2 && displayPage === 0) {
+      displayPage = document.getElementById("productCatalog");
+      displayPage.style.display = "none";
+      displayPage = document.getElementById("cart");
+      displayPage.style.display = "block";
+    } 
+    else if(pageNum ===2){
+      displayPage.style.display = "none";
+      displayPage = document.getElementById("cart");
+      displayPage.style.display = "block";
+    }
+
+    if(pageNum ===3 && displayPage === 0){
+      displayPage = document.getElementById("productCatalog");
+      displayPage.style.display = "none";
+      displayPage = document.getElementById("checkout");
+      displayPage.style.display = "block";
+    }
+    else if(pageNum===3){
+      displayPage.style.display = "none";
+      displayPage = document.getElementById("checkout")
+      displayPage.style.display = "block";
+    }
+  }
+  
+const render_products = (ProductsCategory) => {
   return <div>
-
-  {cartTotal.map((product, index) => (
-    
-<div key={index} class="grid-container" >
-<div class = "grid-item">
-  <img alt="Product Image" src={product.image} />
-  <p>{product.title}</p>
-  <p>${product.price}</p>
+  <h2>Products ({ProductsCategory.length})</h2>
+  <div style={{ maxHeight: '800px', overflowY:
+  'scroll' }}>
   
+  {/* Loop Products */}
+  {ProductsCategory.map((product, index) => (
+  <div key={index} class="grid-container">
+  <div>
+    <img alt="Product Image"src={product.image}/>
+  </div>
+  <div>
+  <div>
+  <h3 className="text-sm text-gray-700">
+  <a href={product.href}>
+  <span aria-hidden="true" className="absolute inset-0" />
+  <span style={{ fontSize: '16px', fontWeight: '600' }}>{product.title}</span>
+  </a>
+  <p>Tag - {product.category}</p>
+  </h3>
+  <p>Rating: {product.rating.rate}</p>
+  </div>
+  <p>${product.price}</p>
   <button onClick={()=> addToCart(product)}> + </button>
-  <button onClick={()=> removeFromCart(product)}> - </button>
-  <p>Count: {counts[product.id-1]}</p>
-</div>
-</div>
+   <button onClick={()=> removeFromCart(product)}> - </button>
+   <p>Count: {counts[product.id-1]}</p>
+  </div>
+  </div>
   ))}
-  <div>{cartItems}</div>
-  </div> 
+  </div>
+  </div>
+  }
+
+  return (
+    <div>
+    <div style={{ minWidth: '65%' }}>
+    <div>
+    <h1> Product Catalog App </h1>
+    <input type="search" value={query} onChange={search}/>
+  <button onClick={()=> pageSwap(1)}> Catalogue </button>
+  <button onClick={()=> pageSwap(2)}> Cart </button>
+  <button onClick={()=> pageSwap(3)}> Checkout </button>
+    <p>
+    <b style={{ color: 'teal' }}>Carter Cutsforth and Keenan Jacobs</b>
+    </p>
+    </div>
+    </div>
+    <div id="productCatalog">
+    {console.log("Before render :",Products.length,cartTotal.length)}
+    {render_products(cartTotal)}
+    </div>
+    <div id="cart">
+      <p>Cha cha changes</p>
+    </div>
+    <div id="checkout">
+      <p>Pages Swap now after 3</p>
+    </div>
+    </div>
+    );
+
   }
 
