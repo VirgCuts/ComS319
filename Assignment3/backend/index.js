cfebb58d86c13ed191e35fefc1b4e85cebc1e86f
VirgCuts <cvcuts@iastate.edu>
2023-04-26 11:53:58 -0500

    added picture functionality need to fix mongo server upload.

--- Assignment3/backend/index.js
+++ Assignment3/backend/index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("public"));
+app.use("/images", express.static("iamges"));
 mongoose.connect("mongodb://127.0.0.1:27017/reactdata",
 {
 dbName: "reactdata",
