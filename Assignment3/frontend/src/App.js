647b5bb3495e4acee9b0e5c394683771bdaa69c5
VirgCuts <cvcuts@iastate.edu>
2023-04-30 16:23:35 -0500

    Now a working implementation needs to get database working next.

--- Assignment3/frontend/src/App.js
+++ Assignment3/frontend/src/App.js
@@ -4,9 +4,158 @@ function App() {
   const [viewer1, setViewer1] = useState(false);
   const [oneProduct, setOneProduct] = useState([]);
   const [viewer2, setViewer2] = useState(false);
+  const [viewer4, setViewer4] = useState(false);
+  const [checked4, setChecked4] = useState(false);
+  const [index, setIndex] = useState(0);
+  
+  function getOneProduct(id) {
+    console.log(id);
+    if (id >= 1 && id <= 20) {
+    fetch("http://localhost:4000/" + id)
+    .then((response) => response.json())
+    .then((data) => {
+    console.log("Show one product :", id);
+    console.log(data);
+    const dataArr = [];
+    dataArr.push(data);
+    setOneProduct(dataArr);
+    });
+    setViewer2(!viewer2);
+    } else {
+    console.log("Wrong number of Product id.");
+    }
+    }
+  
+    const showOneItem = oneProduct.map((el) => (
+      <div key={el._id}>
+      <img src={el.image} width={30} /> <br />
+      Title: {el.title} <br />
+      Category: {el.category} <br />
+      Price: {el.price} <br />
+      Rate :{el.rating.rate} and Count:{el.rating.count} <br />
+      </div>
+      ));
+      
+  
+  function getAllProducts() {
+    fetch("http://localhost:4000/")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Show Catalog of Products :");
+        console.log(data);
+        setProduct(data);
+      });
+    setViewer1(!viewer1);
+  }
+  
+  const showAllItems = product.map((el) => (
+    <div key={el._id}>
+      <img src={el.image} width={30} /> <br />
+      Title: {el.title} <br />
+      Category: {el.category} <br />
+      Price: {el.price} <br />
+      Rate :{el.rating.rate} and Count:{el.rating.count} <br />
+    </div>
+  ));
+  
+  // new Product
+  const [addNewProduct, setAddNewProduct] = useState({
+    _id: 0,
+    title: "",
+    price: 0.0,
+    description: "",
+    category: "",
+    image: "http://127.0.0.1:4000/images/",
+    rating: { rate: 0.0, count: 0 },
+  });
+  
+  function handleChange(evt) {
+    const value = evt.target.value;
+    if (evt.target.name === "_id") {
+      setAddNewProduct({ ...addNewProduct, _id: value });
+    } else if (evt.target.name === "title") {
+      setAddNewProduct({ ...addNewProduct, title: value });
+    } else if (evt.target.name === "price") {
+      setAddNewProduct({ ...addNewProduct, price: value });
+    } else if (evt.target.name === "description") {
+      setAddNewProduct({ ...addNewProduct, description: value });
+    } else if (evt.target.name === "category") {
+      setAddNewProduct({ ...addNewProduct, category: value });
+    } else if (evt.target.name === "image") {
+      const temp = value;
+      setAddNewProduct({ ...addNewProduct, image: temp });
+    } else if (evt.target.name === "rate") {
+      setAddNewProduct({ ...addNewProduct, rating: { rate: value } });
+    } else if (evt.target.name === "count") {
+      const temp = addNewProduct.rating.rate;
+      setAddNewProduct({
+        ...addNewProduct,
+        rating: { rate: temp, count: value },
+      });
+    }
+  }
+  
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+    fetch("http://localhost:4000/insert", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(addNewProduct),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Post a new product completed");
+        console.log(data);
+        if (data) {
+          //const keys = Object.keys(data);
+          const value = Object.values(data);
+          alert(value);
+        }
+      });
+  }
+  
+  function getOneByOneProductNext() {
+    if (product.length > 0) {
+      if (index === product.length - 1) setIndex(0);
+      else setIndex(index + 1);
+      if (product.length > 0) setViewer4(true);
+      else setViewer4(false);
+    }
+  }
+  
+  function getOneByOneProductPrev() {
+    if (product.length > 0) {
+      if (index === 0) setIndex(product.length - 1);
+      else setIndex(index - 1);
+      if (product.length > 0) setViewer4(true);
+      else setViewer4(false);
+    }
+  }
+  
+  function deleteOneProduct(deleteid) {
+    console.log("Product to delete :", deleteid);
+    fetch("http://localhost:4000/delete/", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ _id: deleteid }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Delete a product completed : ", deleteid);
+        console.log(data);
+        if (data) {
+          //const keys = Object.keys(data);
+          const value = Object.values(data);
+          alert(value);
+        }
+      });
+    setChecked4(!checked4);
+  }
+
   return (
     <div>
-      <h1>Catalog of Products</h1>
+      <h1>Catalog ofS Products</h1>
 
       <button onClick={() => getAllProducts()}>Shows All users</button>
 
@@ -111,158 +260,5 @@ function App() {
     </div>
   );
 } // App end
+
 export default App;
-
-const [checked4, setChecked4] = useState(false);
-const [index, setIndex] = useState(0);
-
-function getOneProduct(id) {
-  console.log(id);
-  if (id >= 1 && id <= 20) {
-  fetch("http://localhost:4000/" + id)
-  .then((response) => response.json())
-  .then((data) => {
-  console.log("Show one product :", id);
-  console.log(data);
-  const dataArr = [];
-  dataArr.push(data);
-  setOneProduct(dataArr);
-  });
-  setViewer2(!viewer2);
-  } else {
-  console.log("Wrong number of Product id.");
-  }
-  }
-
-  const showOneItem = oneProduct.map((el) => (
-    <div key={el._id}>
-    <img src={el.image} width={30} /> <br />
-    Title: {el.title} <br />
-    Category: {el.category} <br />
-    Price: {el.price} <br />
-    Rate :{el.rating.rate} and Count:{el.rating.count} <br />
-    </div>
-    ));
-    
-
-function getAllProducts() {
-  fetch("http://localhost:4000/")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Show Catalog of Products :");
-      console.log(data);
-      setProduct(data);
-    });
-  setViewer1(!viewer1);
-}
-
-const showAllItems = product.map((el) => (
-  <div key={el._id}>
-    <img src={el.image} width={30} /> <br />
-    Title: {el.title} <br />
-    Category: {el.category} <br />
-    Price: {el.price} <br />
-    Rate :{el.rating.rate} and Count:{el.rating.count} <br />
-  </div>
-));
-
-useEffect(() => {
-  getAllProducts();
-}, []);
-useEffect(() => {
-  getAllProducts();
-}, [checked4]);
-
-// new Product
-const [addNewProduct, setAddNewProduct] = useState({
-  _id: 0,
-  title: "",
-  price: 0.0,
-  description: "",
-  category: "",
-  image: "http://127.0.0.1:4000/images/",
-  rating: { rate: 0.0, count: 0 },
-});
-
-function handleChange(evt) {
-  const value = evt.target.value;
-  if (evt.target.name === "_id") {
-    setAddNewProduct({ ...addNewProduct, _id: value });
-  } else if (evt.target.name === "title") {
-    setAddNewProduct({ ...addNewProduct, title: value });
-  } else if (evt.target.name === "price") {
-    setAddNewProduct({ ...addNewProduct, price: value });
-  } else if (evt.target.name === "description") {
-    setAddNewProduct({ ...addNewProduct, description: value });
-  } else if (evt.target.name === "category") {
-    setAddNewProduct({ ...addNewProduct, category: value });
-  } else if (evt.target.name === "image") {
-    const temp = value;
-    setAddNewProduct({ ...addNewProduct, image: temp });
-  } else if (evt.target.name === "rate") {
-    setAddNewProduct({ ...addNewProduct, rating: { rate: value } });
-  } else if (evt.target.name === "count") {
-    const temp = addNewProduct.rating.rate;
-    setAddNewProduct({
-      ...addNewProduct,
-      rating: { rate: temp, count: value },
-    });
-  }
-}
-
-function handleOnSubmit(e) {
-  e.preventDefault();
-  console.log(e.target.value);
-  fetch("http://localhost:4000/insert", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(addNewProduct),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Post a new product completed");
-      console.log(data);
-      if (data) {
-        //const keys = Object.keys(data);
-        const value = Object.values(data);
-        alert(value);
-      }
-    });
-}
-
-function getOneByOneProductNext() {
-  if (product.length > 0) {
-    if (index === product.length - 1) setIndex(0);
-    else setIndex(index + 1);
-    if (product.length > 0) setViewer4(true);
-    else setViewer4(false);
-  }
-}
-
-function getOneByOneProductPrev() {
-  if (product.length > 0) {
-    if (index === 0) setIndex(product.length - 1);
-    else setIndex(index - 1);
-    if (product.length > 0) setViewer4(true);
-    else setViewer4(false);
-  }
-}
-function deleteOneProduct(deleteid) {
-  console.log("Product to delete :", deleteid);
-  fetch("http://localhost:4000/delete/", {
-    method: "DELETE",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ _id: deleteid }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Delete a product completed : ", deleteid);
-      console.log(data);
-      if (data) {
-        //const keys = Object.keys(data);
-        const value = Object.values(data);
-        alert(value);
-      }
-    });
-  setChecked4(!checked4);
-}
