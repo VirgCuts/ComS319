fd3a603b2b55e03825b6650c48373716f6f1bf6e
VirgCuts <cvcuts@iastate.edu>
2023-04-30 16:13:34 -0500

    Added some stuff to App and some images

--- Assignment3/frontend/src/App.js
+++ Assignment3/frontend/src/App.js
@@ -2,13 +2,24 @@ import { useState, useEffect } from "react";
 function App() {
   const [product, setProduct] = useState([]);
   const [viewer1, setViewer1] = useState(false);
+  const [oneProduct, setOneProduct] = useState([]);
+  const [viewer2, setViewer2] = useState(false);
   return (
     <div>
       <h1>Catalog of Products</h1>
+
+      <button onClick={() => getAllProducts()}>Shows All users</button>
+
+      <input type="text" id="message" name="message" placeholder="id" onChange={(e) =>getOneProduct(e.target.value)} />
+      
       <button onClick={() => getAllProducts()}>Show All products</button>
       <h1>Show all available Products.</h1>
       <hr></hr>
+
       {viewer1 && <div>Products {showAllItems}</div>}
+      <hr></hr>
+      <h1>Show one Product by Id:</h1>
+      {viewer2 && <div>Product: {showOneItem}</div>}
       <hr></hr>
 
       <h3>Add a new product :</h3>
@@ -102,11 +113,37 @@ function App() {
 } // App end
 export default App;
 
-const [product, setProduct] = useState([]);
-const [viewer1, setViewer1] = useState(false);
-const [oneProduct, setOneProduct] = useState([]);
 const [checked4, setChecked4] = useState(false);
 const [index, setIndex] = useState(0);
+
+function getOneProduct(id) {
+  console.log(id);
+  if (id >= 1 && id <= 20) {
+  fetch("http://localhost:4000/" + id)
+  .then((response) => response.json())
+  .then((data) => {
+  console.log("Show one product :", id);
+  console.log(data);
+  const dataArr = [];
+  dataArr.push(data);
+  setOneProduct(dataArr);
+  });
+  setViewer2(!viewer2);
+  } else {
+  console.log("Wrong number of Product id.");
+  }
+  }
+
+  const showOneItem = oneProduct.map((el) => (
+    <div key={el._id}>
+    <img src={el.image} width={30} /> <br />
+    Title: {el.title} <br />
+    Category: {el.category} <br />
+    Price: {el.price} <br />
+    Rate :{el.rating.rate} and Count:{el.rating.count} <br />
+    </div>
+    ));
+    
 
 function getAllProducts() {
   fetch("http://localhost:4000/")
